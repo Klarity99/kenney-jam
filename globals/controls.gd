971cf8ee0ebad903c64639cbdd1d2b9d70383125
Suhaib Rehman <sohaibabbasi268@gmail.com>
2026-07-18 19:31:11 +0500

    Added textures for bees and crabs

--- globals/controls.gd
+++ globals/controls.gd
@@ -3,6 +3,7 @@ extends Node
 var selected_unit: Unit
 
 func select_unit(new_selected_unit: Unit):
+	print("selected", new_selected_unit)
 	if selected_unit:
 		selected_unit.select_view.visible = false
 	selected_unit = new_selected_unit
